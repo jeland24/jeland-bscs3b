1e6b4b6a50a2372f2d78ae365df98afb8c91d0bc
jeland24 <120576447+jeland24@users.noreply.github.com>
2024-05-21 05:34:56 +0000

    change defi

--- pages/Hashing.py
+++ pages/Hashing.py
@@ -18,17 +18,17 @@ if hash_type == "MD5":
 elif hash_type == "SHA1":
     st.write("""
     ### SHA1 Hash:
-    SHA-1 (Secure Hash Algorithm 1) is a cryptographic hash function that produces a 160-bit (20-byte) hash value. Like MD5, SHA-1 is also widely used to verify data integrity. However, SHA-1 is also not collision-resistant and is considered to be less secure than SHA-256 and SHA-512.
+    SHA-1 (Secure Hash Algorithm 1) is a cryptographic hash function that produces a 160-bit (20-byte) hash value. It was designed by the National Security Agency (NSA) and published by the National Institute of Standards and Technology (NIST) in 1993. SHA-1 was widely used for various security applications, but its security has been compromised due to vulnerabilities.
     """)
 elif hash_type == "SHA256":
     st.write("""
     ### SHA256 Hash:
-    SHA-256 (Secure Hash Algorithm 256-bit) is a cryptographic hash function that produces a 256-bit (32-byte) hash value. It is a part of the SHA-2 family of hashing algorithms and is considered to be more secure than MD5 and SHA-1.
+    SHA-256 (Secure Hash Algorithm 256) is a cryptographic hash function that belongs to the SHA-2 family of hash functions. It produces a 256-bit (32-byte) hash value, making it more secure than its predecessors SHA-1 and MD5. SHA-256 was designed by the National Security Agency (NSA) and published by the National Institute of Standards and Technology (NIST) in 2001.
     """)
 elif hash_type == "SHA512":
     st.write("""
     ### SHA512 Hash:
-    SHA-512 (Secure Hash Algorithm 512-bit) is a cryptographic hash function that produces a 512-bit (64-byte) hash value. It is also a part of the SHA-2 family and is more secure than SHA-256, especially for longer messages.
+    SHA-512 (Secure Hash Algorithm 512) is a cryptographic hash function that belongs to the SHA-2 family of hash functions. It produces a 512-bit (64-byte) hash value, making it more secure than its predecessors SHA-1 and MD5, and providing a larger hash size for increased resistance against brute-force attacks. SHA-512 was designed by the National Security Agency (NSA) and published by the National Institute of Standards and Technology (NIST) in 2001.
     """)
 
 st.write("""
